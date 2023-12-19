b7239700a239f6404fc9c0e2dd414f788a25408b
JkDev <jkdevarg@gmail.com>
2023-12-18 21:56:05 -0500

    Docker Laravel

---
+++ laravel/app/Http/Controllers/NewsController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\News;
+use Illuminate\Http\Request;
+
+class NewsController extends Controller
+{
+    public function index()
+    {
+        $news = News::orderBy('created_at', 'desc')->take(50)->get();
+        
+        return response()->json($news);
+    }
+}
---
+++ laravel/routes/api.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\AuthController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::controller(AuthController::class)->group(function () {
+    Route::post('login', 'login');
+    Route::post('register', 'register');
+    Route::post('logout', 'logout');
+    Route::post('refresh', 'refresh');
+});
